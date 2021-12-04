d3e4073aecdf1b6d32f8fc4550b90122704f3237
simonmader17 <seticher.mader@gmail.com>
2021-12-04 15:32:30 +0100

    Optimized polybar startup

--- my-polybar/launch.sh
+++ my-polybar/launch.sh
@@ -17,6 +17,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+sleep 1
+
 if type "xrandr"; then
 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
 		MONITOR=$m polybar --reload top &
