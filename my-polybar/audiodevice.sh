3f2b43648f6f9de230562b67149ea50a8d4a057b
simonmader17 <seticher.mader@gmail.com>
2021-12-08 12:08:35 +0100

    Optimized audiodevice.sh

--- my-polybar/audiodevice.sh
+++ my-polybar/audiodevice.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-pacmd list-sinks | grep "* index" -A1 | grep "name" | cut -d "." -f 2 | cut -c 1-10 | awk '{print $1"..."}'
+# pacmd list-sinks | grep "* index" -A1 | grep "name" | cut -d "." -f 2 | cut -c 1-10 | awk '{print $1"..."}'
+pacmd list-sinks | grep "* index" -A70 | grep "alsa.long_card_name" | cut -d "\"" -f 2 | cut -c 1-13 | awk '{print $0"..."}'
