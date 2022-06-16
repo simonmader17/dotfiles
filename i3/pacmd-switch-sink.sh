b484c5be1cc46cf8d4a0ee462e2696a3e19919d4
simonmader17 <seticher.mader@gmail.com>
2022-06-16 16:17:47 +0200

    Changed pacmd switch sink script

--- i3/pacmd-switch-sink.sh
+++ i3/pacmd-switch-sink.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-SPEAKER_INDEX=$(pacmd list-sinks | grep -m1 -B1 "analog-stereo" | grep "index" | cut -d":" -f2 | cut -d" " -f2)
+SPEAKER_INDEX=$(pacmd list-sinks | grep -m1 -B1 "pci.*analog-stereo" | grep "index" | cut -d":" -f2 | cut -d" " -f2)
 HEADSET_INDEX=$(pacmd list-sinks | grep -m1 -B1 "usb-Logitech_G433_Gaming_Headset" | grep "index" | cut -d":" -f2 | cut -d" " -f2)
 
 echo $SPEAKER_INDEX
 echo $HEADSET_INDEX
 
-if pacmd list-sinks | grep -m1 -B1 "analog-stereo" | grep -q "* index";
+if pacmd list-sinks | grep -m1 -B1 "pci.*analog-stereo" | grep -q "* index";
 then
 	# If speaker is selected, switch to headset
 	pacmd set-default-sink $HEADSET_INDEX
