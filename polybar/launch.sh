73b91513890e21066a33f7767299981b55a482f3
simonmader17 <seticher.mader@gmail.com>
2022-09-23 08:15:15 +0200

    Updated polybar launch file

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -19,13 +19,26 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 sleep 1
 
+monitors=$(xrandr --query | grep " connected" | cut -d" " -f1)
+echo $monitors
+
+largeMonitorExists=false
+
 if type "xrandr"; then
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+	# Check if large monitor exists
+	for m in $monitors; do
+		if [[ "$(xrandr --query | grep $m)" == *"2560x1440"* ]]; then
+			largeMonitorExists=true
+		fi
+	done
+	for m in $monitors; do
 		if [[ "$(xrandr --query | grep $m)" == *"2560x1440"* ]]; then
 			MONITOR=$m polybar --reload top &
 		else
 			MONITOR=$m polybar --reload top-small &
-			MONITOR=$m polybar --reload bottom-small &
+			if [ "$largeMonitorExists" = false ]; then
+				MONITOR=$m polybar --reload bottom-small &
+			fi
 		fi
 	done
 else
