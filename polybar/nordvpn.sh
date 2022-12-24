6d91b818428e4b893054c316f2f2a35c47a43596
simonmader17 <seticher.mader@gmail.com>
2022-12-25 00:49:10 +0100

    Fixed polybar nordvpn script

--- polybar/nordvpn.sh
+++ polybar/nordvpn.sh
@@ -4,5 +4,5 @@ if [[ $(nordvpn status | grep "Status: Disconnected") ]]
 then
 	echo "<Connect to VPN>"
 else
-	nordvpn status | grep "Status: Connected" -A4 | grep "IP" | cut -d " " -f 3
+	nordvpn status | grep "Status: Connected" -A4 | grep "IP" | cut -d " " -f 2
 fi
