0fbf9683a0af7652c1ea6714eea33c230ec4fa72
simonmader17 <mail@simonmader.at>
2023-08-25 12:51:59 +0200

    Updated polybar launch script, so that primary monitor always gets system tray

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -42,6 +42,9 @@ if type "xrandr"; then
 				MONITOR=$m polybar --reload bottom-small &
 			fi
 		fi
+		if [[ "$m" == "$primary" ]]; then
+			sleep 1 # Wait for primary monitor to grab the tray
+		fi
 	done
 else
 	polybar --reload top &
