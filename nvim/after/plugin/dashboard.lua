69d61f3a3c7d8a0e59cbfb045fa2289dbe2615de
simonmader17 <mail@simonmader.at>
2023-12-25 00:16:14 +0100

    nvim: updated dashboard

--- nvim/after/plugin/dashboard.lua
+++ nvim/after/plugin/dashboard.lua
@@ -1,1 +1,44 @@
-require 'alpha'.setup(require 'alpha.themes.startify'.config)
+local alpha = require("alpha")
+local theme = require("alpha.themes.theta")
+
+local my_art = {
+	ansi_shadow = {
+		"███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗",
+		"████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║",
+		"██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║",
+		"██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║",
+		"██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║",
+		"╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝"
+	},
+	pikachu = {
+		"",
+		"          ▀████▀▄▄              ▄█ ",
+		"            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ",
+		"    ▄        █          ▀▀▀▀▄  ▄▀  ",
+		"   ▄▀ ▀▄      ▀▄              ▀▄▀  ",
+		"  ▄▀    █     █▀   ▄█▀▄      ▄█    ",
+		"  ▀▄     ▀▄  █     ▀██▀     ██▄█   ",
+		"   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ",
+		"    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ",
+		"   █   █  █      ▄▄           ▄▀   ",
+	},
+}
+
+theme.header.val = my_art.pikachu
+
+-- lolcat header:
+--[[ require("alpha.term")
+local my_header = {
+	type = "terminal",
+	command = '/usr/bin/figlet -f "ANSI Shadow" "Neovim" | lolcat',
+	width = 50,
+	height = 7,
+	opts = {
+		redraw = true,
+		window_config = {}
+	}
+}
+theme.header = my_header
+theme.config.layout[2] = my_header ]]
+
+alpha.setup(theme.config)
