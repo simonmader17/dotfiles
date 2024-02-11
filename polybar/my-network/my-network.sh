f8fcd587d63d0c29d654a88f0d89eb2f6d0ba222
simonmader17 <mail@simonmader.at>
2024-02-11 18:24:57 +0100

    polybar: updated my-network module

--- polybar/my-network/my-network.sh
+++ polybar/my-network/my-network.sh
@@ -2,6 +2,7 @@
 local_interface=$(route | awk '/^default/{print $NF}')
 if [ -z "$local_interface" ]; then
 	echo "󰤣 Not connected"
+	echo "<Show IP>" > ~/dotfiles/polybar/my-network/current # Reset current file
 else
 	cat ~/dotfiles/polybar/my-network/current
 fi
