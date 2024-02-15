5745b360407f46fb1f2553152a655f87ef3f203a
simonmader17 <mail@simonmader.at>
2024-02-16 00:28:11 +0100

    .

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -12,7 +12,7 @@
 # MONITORS=$MONITORS polybar top
 
 # Terminate already running bar instances
-killall -q polybar
+killall -q polybar polybar-msg
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
