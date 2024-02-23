3150b40f0905ca3272fa9892d41ae11dab4d8c91
simonmader17 <mail@simonmader.at>
2024-02-23 03:35:07 +0100

    .

--- scripts/pywal/post-pywal.sh
+++ scripts/pywal/post-pywal.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Add transparency to background
+# Add transparency to background and color6
 # dunstify -h string:x-dunst-stack-tag:wallpaper-setter -i ~/dotfiles/scripts/pywal/pywal-icon.png "Wallpaper Setter" "Adding transparency to background for Polybar."
 background="$(xrdb -get background)"
 echo "*background-transparent: ${background:0:1}AA${background:1}" >> ~/.cache/wal/colors.Xresources
+color6="$(xrdb -get color6)"
+echo "*color6-transparent: ${color6:0:1}CC${color6:1}" >> ~/.cache/wal/colors.Xresources
 xrdb -merge ~/.cache/wal/colors.Xresources
 
 WALLPAPER="$(cat ~/.cache/wal/colors.json | jq ".wallpaper" -r)"
