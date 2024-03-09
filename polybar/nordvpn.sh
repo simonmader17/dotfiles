293420f22fa3f288a27ea1a6025d5ca1bae28439
simonmader17 <mail@simonmader.at>
2024-03-09 23:32:55 +0100

    .

--- polybar/nordvpn.sh
+++ polybar/nordvpn.sh
@@ -4,5 +4,8 @@ if [[ $(nordvpn status | grep "Status: Disconnected") ]]
 then
 	echo "<Connect to VPN>"
 else
-	nordvpn status | grep "Status: Connected" -A4 | grep "IP" | cut -d " " -f 2
+	# nordvpn status | grep "Status: Connected" -A4 | grep "IP" | cut -d " " -f 2
+	ip=$(nordvpn status | grep -P '^IP' | cut -d " " -f 2)
+	country=$(nordvpn status | grep -P '^Country' | cut -d " " -f 2-)
+	echo "$ip ($country)"
 fi
