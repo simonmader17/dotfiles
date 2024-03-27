a9483d487241473b9e6e125fb8f5c195a9251a74
simonmader17 <mail@simonmader.at>
2024-03-27 23:22:32 +0100

    Added nvim vscode config

--- nvim/lua/simon/init.lua
+++ nvim/lua/simon/init.lua
@@ -2,4 +2,8 @@ require("simon.set")
 require("simon.secrets") -- Includes secrets like vim.g.codestats_api_key
 require("simon.vscode")
 require("simon.remap")
-require("simon.packer")
+if vim.g.vscode then
+	require("simon.packer-vscode")
+else
+	require("simon.packer")
+end
