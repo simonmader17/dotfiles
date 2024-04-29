256dec8d54877933f439718ca763bea98f43a21f
simonmader17 <mail@simonmader.at>
2024-04-29 12:42:04 +0200

    nvim: changed tabstop size

--- nvim/lua/simon/set.lua
+++ nvim/lua/simon/set.lua
@@ -10,9 +10,9 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 -- Tab
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.breakindent = true
