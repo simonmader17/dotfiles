be94ca60913b0a23c0c346844c9f4a5577f7cc23
simonmader17 <mail@simonmader.at>
2024-09-12 18:53:07 +0200

    set-wallpaper.sh: fixed notifications

--- scripts/pywal/set-wallpaper.sh
+++ scripts/pywal/set-wallpaper.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-dunstify -h string:x-dunst-stack-tag:wallpaper-setter -i ~/dotfiles/scripts/pywal/pywal-icon.png "Wallpaper Setter" "Setting \"$1\" as wallpaper..."
+
+swaync_tag='string:x-canonical-private-synchronous:wallpaper-setter'
+dunst_tag='string:x-dunst-stack-tag:wallpaper-setter'
+
+notify-send -h "$swaync_tag" -h "$dunst_tag" -i ~/dotfiles/scripts/pywal/pywal-icon.png "Wallpaper Setter" "Setting \"$1\" as wallpaper..."
 wal -i "$1" --backend colorz -o ~/dotfiles/scripts/pywal/post-pywal.sh |
 while read -r line; do
-	dunstify -h string:x-dunst-stack-tag:wallpaper-setter -i ~/dotfiles/scripts/pywal/pywal-icon.png "Wallpaper Setter" "${line##*: }"
+	notify-send -h "$swaync_tag" -h "$dunst_tag" -i ~/dotfiles/scripts/pywal/pywal-icon.png "Wallpaper Setter" "${line##*: }"
 done
 
 # On wayland
@@ -12,6 +16,6 @@ done
 # fi
 
 if [ "${PIPESTATUS[0]}" = 0 ]
-then dunstify -h string:x-dunst-stack-tag:wallpaper-setter -i ~/dotfiles/scripts/pywal/pywal-icon.png "Wallpaper Setter" "Successfully set wallpaper."
-else dunstify -h string:x-dunst-stack-tag:wallpaper-setter -i ~/dotfiles/scripts/pywal/pywal-icon.png -u critical "Wallpaper Setter" "Failed to set wallpaper."
+then notify-send -h "$swaync_tag" -h "$dunst_tag" -i ~/dotfiles/scripts/pywal/pywal-icon.png "Wallpaper Setter" "Successfully set wallpaper."
+else notify-send -h "$swaync_tag" -h "$dunst_tag" -i ~/dotfiles/scripts/pywal/pywal-icon.png -u critical "Wallpaper Setter" "Failed to set wallpaper."
 fi
